c2a68ec5b869898f39345a2301df5837fc36c51d
kunxin-chor <chorkunxin@yahoo.com>
2024-01-10 11:30:36 +0000

    Added airbnb demo

---
+++ 03-mongo-express/airbnb_demo.js
@@ -0,0 +1,22 @@
+// require in the MongoDB client
+const MongoClient = require("mongodb").MongoClient;
+const mongoURL = "mongodb+srv://root:rotiprata123@cluster0.cacnq.mongodb.net/?retryWrites=true&w=majority";
+
+// we are going to use `await` to wait for some
+// asynchronous operation to finish
+async function main() {
+ // create the mongo client
+ // the conenction function has two parameters
+ // the first parameter: connection string (get from your Mongo Atlas)
+ // the second parameter: options
+ const client = await MongoClient.connect(mongoURL, {
+    "useUnifiedTopology": true // it's for using the latest version of MongoDB
+ });
+ // connection
+ const db = client.db("sample_airbnb");
+ console.log("database connected");
+ // mongoshell: db.listingsAndReviews.find().limit(10)
+ const listings = await db.collection("listingsAndReviews").find().limit(10).toArray();
+ console.log(listings);
+}
+main();
